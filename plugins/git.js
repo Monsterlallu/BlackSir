c8fcc9d3a58d42a732751d0e95a24c5938a46252
C7836 M0N5T3R L4LLU <87799191+Monsterlallu@users.noreply.github.com>
2021-12-13 00:28:14 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -10,14 +10,14 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
     var r_text = new Array ();
     
     
-    r_text[0] = "https://i.ibb.co/Y2mXdR5/ajuser.jpg";
+    r_text[0] = "https://www.linkpicture.com/q/PicsArt_12-13-12.20.42.jpg";
     
     
     var i = Math.floor(1*Math.random())
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `ＡＪＵＳＥＲ ＢＯＴ
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `ＢＬＡＣＫ ＳＩＲ
 
 *bot making video* : _https://youtu.be/MPoRRTkjuW0_
 
